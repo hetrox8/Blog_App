65eaea3310fa94b99bc911ffe574adb51d0eb445
hetrox8 <dretrevor8@gmail.com>
2023-10-26 22:22:03 +0300

    create custom methods, insert data via models

---
+++ app/models/comment.rb
@@ -0,0 +1,12 @@
+class Comment < ApplicationRecord
+  belongs_to :post, foreign_key: 'post_id'
+  belongs_to :author, class_name: 'User'
+
+  validates :text, presence: true
+
+  after_create :increment_post_comments_counter
+
+  def increment_post_comments_counter
+    post.increment!(:comments_counter, 1)
+  end
+end
