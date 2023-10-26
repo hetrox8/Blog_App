2b03437f9fa0351686e1a77b7bcb3e0e0c8bc9c2
suleiman gacheru <dretrevor8@gmail.com>
2023-10-27 01:26:49 +0300

    make use of after save

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  after_create :update_post_counter
+  after_save :update_post_counter
 
   def update_post_counter
     author.increment!(:posts_counter)
