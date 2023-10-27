17905825a693ceb67c382426ac573e934008d2cf
hetrox8 <dretrevor8@gmail.com>
2023-10-27 20:00:26 +0300

    solve rubocop 3

--- db/schema.rb
+++ db/schema.rb
@@ -1,17 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema[7.1].define(version: 202_310_261_812_49) do
-  # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
   create_table 'comments', force: :cascade do |t|
@@ -20,8 +7,6 @@ ActiveRecord::Schema[7.1].define(version: 202_310_261_812_49) do
     t.bigint 'author_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.index ['author_id'], name: 'index_comments_on_author_id'
-    t.index ['post_id'], name: 'index_comments_on_post_id'
   end
 
   create_table 'likes', force: :cascade do |t|
@@ -29,8 +14,6 @@ ActiveRecord::Schema[7.1].define(version: 202_310_261_812_49) do
     t.bigint 'author_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.index ['author_id'], name: 'index_likes_on_author_id'
-    t.index ['post_id'], name: 'index_likes_on_post_id'
   end
 
   create_table 'posts', force: :cascade do |t|
@@ -41,7 +24,6 @@ ActiveRecord::Schema[7.1].define(version: 202_310_261_812_49) do
     t.bigint 'author_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.index ['author_id'], name: 'index_posts_on_author_id'
   end
 
   create_table 'users', force: :cascade do |t|
@@ -52,6 +34,12 @@ ActiveRecord::Schema[7.1].define(version: 202_310_261_812_49) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
+
+  add_index 'comments', ['author_id'], name: 'index_comments_on_author_id'
+  add_index 'comments', ['post_id'], name: 'index_comments_on_post_id'
+  add_index 'likes', ['author_id'], name: 'index_likes_on_author_id'
+  add_index 'likes', ['post_id'], name: 'index_likes_on_post_id'
+  add_index 'posts', ['author_id'], name: 'index_posts_on_author_id'
 
   add_foreign_key 'comments', 'posts'
   add_foreign_key 'comments', 'users', column: 'author_id'
