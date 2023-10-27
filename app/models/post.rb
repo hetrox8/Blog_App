fc90958fa0c028f4f73f4a2167370bb1202e9a5b
hetrox8 <dretrevor8@gmail.com>
2023-10-27 19:21:54 +0300

    add rspec test and ensure all pass

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes, counter_cache: true
-  has_many :comments
+  has_many :comments, foreign_key: 'post_id'
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  after_save :update_post_counter
+  after_create :update_post_counter
 
   def update_post_counter
     author.increment!(:posts_counter)
