a48fcb992b0511012e893aa87a77cc2359d77d99
suleiman gacheru <dretrevor8@gmail.com>
2023-10-30 19:33:29 +0300

    add routers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,13 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.includes(:comments).paginate(page: params[:page], per_page: 5)
+    @posts = User.post.includes(:comments).paginate(page: params[:page], per_page: 5)
   end
 
   def show
-    @user = User.find(params[:user_id])
-    @post = @user.posts.find(params[:id])
+    @user = User.find(params[:id])
+    @post = @user.post.fing(params[:id])
     @comments = @post.comments.includes(:author).paginate(page: params[:page], per_page: 10)
-    @likes_count = @post.likes.count
-    @comments_count = @post.comments.count
+    @comments_count = @post.likes.size
   end
 end
