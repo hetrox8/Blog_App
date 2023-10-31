5a03700d848750aa6d7644db486ff5d2de825f14
suleiman gacheru <dretrevor8@gmail.com>
2023-10-31 20:53:42 +0300

    solve rubocop 2

--- spec/request/users_controller_spec.rb
+++ spec/request/users_controller_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe UsersController, type: :request do
       get '/users'
 
       expect(assigns(:users)).to eq([user1, user2])
-      expect(response).to have_http_status(200)            # Check response status
-      expect(response).to render_template(:index)           # Check correct template rendering
+      expect(response).to have_http_status(200) # Check response status
+      expect(response).to render_template(:index) # Check correct template rendering
       expect(response.body).to include('Welcome to user index page') # Check response body content
     end
   end
@@ -22,8 +22,8 @@ RSpec.describe UsersController, type: :request do
       get "/users/#{user.id}"
 
       expect(assigns(:user)).to eq(user)
-      expect(response).to have_http_status(200)            # Check response status
-      expect(response).to render_template(:show)           # Check correct template rendering
+      expect(response).to have_http_status(200) # Check response status
+      expect(response).to render_template(:show) # Check correct template rendering
       expect(response.body).to include('Welcome to user show page') # Check response body content
     end
   end
