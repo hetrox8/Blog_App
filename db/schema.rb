11b3d56af6ca299a60d9e95d9243b53dcf4c3cd5
suleiman gacheru <dretrevor8@gmail.com>
2023-11-02 19:03:05 +0300

    solve rubocop 1

--- db/schema.rb
+++ db/schema.rb
@@ -59,4 +59,3 @@ ActiveRecord::Schema[7.0].define(version: 20_230_511_172_910) do
   add_foreign_key 'likes', 'users', column: 'author_id'
   add_foreign_key 'posts', 'users', column: 'author_id'
 end
-
